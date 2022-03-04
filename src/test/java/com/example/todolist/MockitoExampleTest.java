b37035d1ef8f4167a487545186c3ffb378e6aca4
kyupid <iamscvv@gmail.com>
2022-03-04 22:13:17 +0900

    Mockito when/then 이해하기

--- src/test/java/com/example/todolist/MockitoExampleTest.java
+++ src/test/java/com/example/todolist/MockitoExampleTest.java
@@ -1,9 +1,11 @@
 package com.example.todolist;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.*;
 
 class MockitoExampleTest {
@@ -35,5 +37,37 @@ class MockitoExampleTest {
 
         // 해당 mock이 아예 호출이 안됐는지 검증
         verify(mock, never()).get(2);
+    }
+
+    @Test
+    @DisplayName("when/then 테스트")
+    void whenThen() {
+        class Animal {
+            int age;
+            String name;
+            boolean fly;
+
+            public int getAge() {
+                return age;
+            }
+
+            public String getName() {
+                return name;
+            }
+
+            public boolean isFly() {
+                return fly;
+            }
+        }
+        Animal animal = mock(Animal.class);
+        assertTrue(animal != null);
+
+        when(animal.getAge()).thenReturn(30);
+        when(animal.getName()).thenReturn("바부");
+        when(animal.isFly()).thenReturn(true);
+
+        assertTrue(animal.getAge() == 30);
+        assertTrue(animal.getName().equals("바부"));
+        assertTrue(animal.isFly() == true);
     }
 }
