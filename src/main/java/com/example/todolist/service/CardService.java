055a7199ac7fe5ecbe2fc8d9eb64e7657cf9f0d5
kyupid <iamscvv@gmail.com>
2022-03-14 15:54:44 +0900

    카드 수정 기능 추가

--- src/main/java/com/example/todolist/service/CardService.java
+++ src/main/java/com/example/todolist/service/CardService.java
@@ -4,6 +4,7 @@ import com.example.todolist.domain.Card;
 import com.example.todolist.domain.Column;
 import com.example.todolist.dto.CardAddRequestDTO;
 import com.example.todolist.dto.CardUpdateRequestDto;
+import com.example.todolist.exception.CardNotFoundException;
 import com.example.todolist.exception.ColumnNotFoundException;
 import com.example.todolist.repository.CardRepository;
 import com.example.todolist.repository.ColumnRepository;
@@ -21,5 +22,14 @@ public class CardService {
         Column column = columnRepository.findById(columnId).orElseThrow(ColumnNotFoundException::new);
         Card card = new Card(cardRequest.getTitle(), cardRequest.getContent(), cardRequest.getAuthor(), column);
         cardRepository.save(card);
+    }
+
+    public Card updateCard(long columnId, long cardId, CardUpdateRequestDto cardUpdateRequestDto) {
+        Column column = columnRepository.findById(columnId).orElseThrow(ColumnNotFoundException::new);
+        Card card = cardRepository.findById(cardId).orElseThrow(CardNotFoundException::new);
+
+        card.update(cardUpdateRequestDto.getTitle(), cardUpdateRequestDto.getContent(), column);
+
+        return cardRepository.save(card);
     }
 }
