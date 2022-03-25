fad38a5b66b2f9c60cea3ec784eea08ae34c740e
kyupid <iamscvv@gmail.com>
2022-03-26 00:31:32 +0900

    로그 추가 기능 테스트 통과하도록 추가

--- src/main/java/com/example/todolist/service/LogService.java
+++ src/main/java/com/example/todolist/service/LogService.java
@@ -1,10 +1,20 @@
 package com.example.todolist.service;
 
+import com.example.todolist.domain.Actions;
 import com.example.todolist.domain.Card;
+import com.example.todolist.domain.Log;
+import com.example.todolist.repository.LogRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class LogService {
 
-    public void createLog(Card card) {
+    private final LogRepository logRepository;
 
+    public void createLog(Card card) {
+        Log log = new Log(card.getAuthor(), Actions.ENROLL, card.getColumn(), card.getColumn(), card);
+        logRepository.save(log);
     }
 }
